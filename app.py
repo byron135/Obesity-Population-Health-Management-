50b996308436534b5cbce1868f64512885ab5724
ppunmaneeluk3 <paula.pun@gatech.edu>
2023-04-02 13:16:17 -0400

    Adding static folder and metrics page

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
     return render_template('home.html')
+
+@app.route("/metrics")
+def metrics():
+    return render_template('metrics.html')
 
 
 if __name__ == "__main__":
