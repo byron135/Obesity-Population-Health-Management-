4c91f9e7f6e1b1f677a694508bd42355741447bf
ppunmaneeluk3 <paula.pun@gatech.edu>
2023-04-21 14:26:50 -0400

    Adding html table but the route /table is broken

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, url_for, redirect, request
 from app.src.tools import connectionHelper
+import pandas as pd
 
 app = Flask(__name__)
+a = pd.read_csv("patient.csv")
 
-
-@app.route("/")
+@app.route("/", methods =['GET', 'POST'])
 def home():
+    #html_table = a.to_html()
     return render_template('home.html')
 
 @app.route("/metrics")
@@ -29,6 +31,24 @@ def get_patient():
     connectionHelper.init()
     print("done")
     return "File saved as patient.csv"
+
+@app.route('/table', methods=['GET', 'POST'])
+def display_table():
+    # get the unique values in the 'Category' column
+    categories = a['Category'].unique()
+    # handle form submission
+    if request.method == 'POST':
+        # get the selected category from the form
+        selected_category = request.form['category']
+        # filter the DataFrame based on the selected category
+        filtered_df = a[a['Category'] == selected_category]
+        # convert the filtered DataFrame to an HTML table
+        table_html = filtered_df.to_html(classes='table table-striped')
+        # render the template with the table and the category options
+        return render_template('table.html', table=table_html, categories=categories, selected_category=selected_category)
+    # if no form submission, render the template with the table and the category options
+    table_html = a.to_html(classes='table table-striped')
+    return render_template('table.html', table=table_html, categories=categories, selected_category=None)
 
 if __name__ == "__main__":
     app.run()
