63a2cb5df560fccfa7f47031f22d510b59a52ac8
byron135 <70074940+byron135@users.noreply.github.com>
2023-04-22 16:17:26 -0400

    added filter function

--- app/src/tools/table_operation.py
+++ app/src/tools/table_operation.py
@@ -8,3 +8,15 @@ def filter_patients(df, age_threshold, gender, bmi_threshold):
     """    
     filtered_df = df.query(f"age >= {age_threshold} and gender == '{gender}' and BMI >= {bmi_threshold}")
     return filtered_df
+
+def filter_patients_race(df, age_threshold,race, bmi_threshold):
+
+    filtered_df = df.query(f"race == '{race}' and BMI >= {bmi_threshold} and age >= {age_threshold}")
+
+    return filtered_df
+
+def filter_smoking_status(df, smoking_status, bmi_threshold):
+
+    filtered_df = df.query(f"smoking_status == '{smoking_status}' and BMI >= {bmi_threshold}")
+
+    return filtered_df
